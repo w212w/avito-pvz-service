e193b574eac90f2e7853f6405d94519aa27498e2
w212w <edaev.bilal@icloud.com>
2025-04-10 18:20:48 +0300

    added config

---
+++ config/config.go
@@ -0,0 +1,54 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	JWTSecret  string
+}
+
+func LoadConfig() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file")
+	}
+
+	appEnv := getEnv("APP_ENV", "local")
+
+	if appEnv == "docker" {
+		log.Println("Running in Docker mode, ignoring .env file")
+		return &Config{
+			DBHost:     getEnv("DB_HOST", "db"),
+			DBPort:     getEnv("DB_PORT", "5432"),
+			DBUser:     getEnv("DB_USER", "postgres"),
+			DBPassword: getEnv("DB_PASSWORD", "postgres"),
+			DBName:     getEnv("DB_NAME", "pvz"),
+			JWTSecret:  getEnv("JWT_SECRET", "supersecretkey"),
+		}
+	}
+
+	return &Config{
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+		DBUser:     getEnv("DB_USER", "postgres"),
+		DBPassword: getEnv("DB_PASSWORD", "postgres"),
+		DBName:     getEnv("DB_NAME", "pvz"),
+		JWTSecret:  getEnv("JWT_SECRET", "supersecretkey"),
+	}
+}
+
+func getEnv(key, fallback string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return fallback
+}
